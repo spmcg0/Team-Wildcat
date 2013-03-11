d33fbc9e8293dd5c5f2c3ef4539ce7c51749101b
brettstearns <brettstearns13@gmail.com>
2013-03-11 18:38:12 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ exports.discover = function(req, res){
 };
 
 exports.profile_page = function(req,res){
-	var spongebob = spongebob.getSpongebob();
+	var spongebob = spongebob.getSpongebobInfo();
 	var spongebobTweets = spongebob.getSpongebobTweets();
 	res.render('profile_page', {spongebobInfo: spongebobInfo, spongebobTweets: spongebobTweets});
 };
