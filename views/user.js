44c7f5c9e09c1c87840faff913861815a185c80e
Brian Stapleton <bstapes44@yahoo.com>
2013-03-10 22:59:05 -0400

    Added my files
    added the files for project assignment 2

---
+++ views/user.js
@@ -0,0 +1,11 @@
+var user = {
+	name: "Spongebob Squarepants",
+	tweetCount: "50",
+	followerCount: "100",
+	followingCount: "120"
+};
+
+var tweets = [ 
+{uname:"Patrick", tweet:"I miss mah sponge homie"}, 
+{uname:"Squidward", tweet:"I hate Spongebob"}, 
+{uname:"Mr. Krabs", tweet:"Spongebob is my best employee"}];
