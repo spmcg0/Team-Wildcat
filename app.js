4aa1f643bf025bc99e03614f91630bef5af237fe
brettstearns <brettstearns13@gmail.com>
2013-03-11 17:57:34 -0300

    fixed a type

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/timeline', routes.timeline);
-app.get('/profile_page', routes.proile_page);
+app.get('/profile_page', routes.profile_page);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
