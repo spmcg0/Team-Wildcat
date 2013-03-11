f01d38c301360596ff561e56272c5881e93ec434
brettstearns <brettstearns13@gmail.com>
2013-03-11 20:16:59 -0300

    WORK

--- routes/profile_page.js
+++ routes/profile_page.js
@@ -1,7 +1,7 @@
 var spongebob = require('../lib/spongebob');
 
 exports.profile_page = function(req,res){
-  var spongebobInfo = spongebob.getSpongebobInfo();
+  	var spongebobInfo = spongebob.getSpongebobInfo();
 	var spongebobTweets = spongebob.getSpongebobTweets();
-	res.render('profile_page', {title: 'profile_page',spongebobInfo: spongebobInfo, spongebobTweets: spongebobTweets});
+	res.render('profile_page', {spongebobInfo: spongebobInfo, spongebobTweets: spongebobTweets});
 };
