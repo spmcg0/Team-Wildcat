4735dfc6c82b42e3c77464d91f8c10448efd0058
Sean McGrath <spmcg0@student.umass.edu>
2013-03-29 17:28:55 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,40 @@ exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
+/*
+ * The process for adding a user
+ */
+
+var users = require('../lib/users');
+
+exports.process = function (req, res) {
+  var id   = req.params.id;
+  var user = findUser(req);
+
+  if (users.checkUser(user)) {
+    users.addUser(user); );
+  }
+  else {
+    
+    };
+
+  }
+}
+function userData(req) {
+  var user;
+  if (req.method === 'GET') {
+    user = {
+      username: req.query.username,
+      password : req.query.password,
+
+    };
+  }
+  else {
+    user = {
+      username: req.body.username,
+      password : req.body.pasword,
+    };
+  }
+  
+  return user;
+}
