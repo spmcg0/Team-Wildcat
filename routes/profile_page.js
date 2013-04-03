feba80876a34160eebc5535023a77a664c910af8
Brett Stearns <brettstearns13@gmail.com>
2013-04-02 22:49:58 -0400

    fuck github

--- routes/profile_page.js
+++ routes/profile_page.js
@@ -2,6 +2,13 @@ var spongebob = require('../lib/spongebob');
 
 exports.profile_page = function(req,res){
   	var spongebobInfo = spongebob.getSpongebobInfo();
-	var spongebobTweets = spongebob.getSpongebobTweets();
+  	genTweets(function (ul) {
+      res.render('profile_page',
+                 { title: 'form - ' + id,
+                   id: id,
+                   msg: '',
+                   users: ul });
+    });
+	//var spongebobTweets = spongebob.getSpongebobTweets();
 	res.render('profile_page', {title: 'profile_page',spongebobInfo: spongebobInfo, spongebobTweets: spongebobTweets});
 };
