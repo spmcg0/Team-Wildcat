8f2a9da76cef761e6e102bf48718a28d8aab7f5c
Gregg Mancari <gmancari@student.umass.edu>
2013-04-15 14:44:35 -0400

    Tweet functon added to profilepage

--- routes/profile_page.js
+++ routes/profile_page.js
@@ -5,3 +5,20 @@ exports.profile_page = function(req, res){
   	var tweets = users.getTweets();
 	res.render('profile_page_mock', { tweets: tweets, user: user});
 };
+
+exports.tweet = function (req, res) {
+  var userid = req.cookies.userid;
+  var u = online[userid];
+  var tweet = tweetData(req);
+  if(user.validateUserTweet(tweet)) {
+  	user.addUserTweet(tweet, function(error, tweets){
+  		online[userid]['tweets'] = tweets;
+  	});
+  	u.tweetCount++;
+  	var t = u.tweets;
+  	res.render('profile_page', { tweets : t, user : u });
+  }
+  else{
+    console.log('failed');
+  }
+};
