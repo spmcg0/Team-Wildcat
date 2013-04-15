525944d1f92412765372ec939a054ae70b5f742c
Brian Stapleton <bstapes44@yahoo.com>
2013-04-15 13:26:57 -0400

    created the timeline_update.js for the timeline update websockets

---
+++ public/javascripts/timeline_update.js
@@ -0,0 +1,44 @@
+function publisher() {
+	var subscribers = {};
+	var obj = {};
+
+	obj.subscrive = function (type, fn) {
+		if (subscribers[type] === undefined) {
+			subscribers[type] = [];
+		}
+		subscribers[type].push(fn);
+	};
+
+	obj.unsubscribe = function (type, fn) {
+    if (subscribers[type] === undefined) {
+      return false;
+    }
+    var s = subscribers[type];
+    var i;
+    for (i = 0; i < s.length; i++) {
+      if (s[i] === fn) {
+        delete s[i];
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  obj.publish = function (type, arg) {
+    if (subscribers[type] === undefined) {
+      return false;
+    }
+
+    // Iterates over subscriber functions and invokes them:
+    var s = subscribers[type];
+    var i;
+    for (i = 0; i < s.length; i++) {
+      var fn = s[i];
+      fn(arg);
+    }
+    return true;
+  };
+
+  return obj;
+}
